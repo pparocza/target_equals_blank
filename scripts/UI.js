af63fe51eb2ef9837f05215bc5f5c6c0154ba2de
paparoczai <paulparoczai@gmail.com>
2021-07-31 13:40:49 -0700

    shuffle title

--- scripts/UI.js
+++ scripts/UI.js
@@ -2,8 +2,28 @@ var startButton = document.querySelector('.start');
 var onlineButton = document.querySelector('.online');
 var gainSlider = document.querySelector('.gain-slider');
 var gainDisplay = document.querySelector('.gain');
+var title = document.querySelector('.title');
 var volume = gainSlider.value;
 var displayVolume;
+
+String.prototype.shuffle = function () {
+    var a = this.split(""),
+        n = a.length;
+
+    for(var i = n - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+    return a.join("");
+}
+
+var targetString = "target";
+var equalsString = "equals"
+var blankString = "blank";
+
+title.innerHTML = targetString.shuffle() + ' ' + equalsString.shuffle() + ' ' + blankString.shuffle();
 
 onlineButton.onclick = function(){
 
